e48889d50d167a7d58b823797e5652bbcfd5cbb4
Jimmy <devdjcod@gmail.com>
2026-06-01 23:36:57 -0400

    Corregir tsconfig del frontend para vercel dos

--- chelo/src/utils/jst.ts
+++ chelo/src/utils/jst.ts
@@ -1,15 +1,20 @@
 import jwt from 'jsonwebtoken'
-import Types from 'mongoose'
 
 type UsuarioPayload = {
-    id: string,
-    name:string
+  id: string
+  name: string
 }
+
 export const generateJWT = (payload: UsuarioPayload) => {
+  const jwtSecret = process.env.JWT_SECRET
 
-    const tokenjwt = jwt.sign(payload, process.env.JWT_SECRET, {
-        expiresIn: '180d'
-    })
-    return tokenjwt
+  if (!jwtSecret) {
+    throw new Error('JWT_SECRET no está definido')
+  }
 
+  const tokenjwt = jwt.sign(payload, jwtSecret, {
+    expiresIn: '180d'
+  })
+
+  return tokenjwt
 }
