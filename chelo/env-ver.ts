d17a9a00fc1e65cf7d5f65b69b5e0404455ea318
Jimmy <devdjcod@gmail.com>
2026-06-13 17:55:09 -0400

    env

---
+++ chelo/env-ver.ts
@@ -0,0 +1,15 @@
+// DATABASE_URL=mongodb+srv://devdjcod:A2kElS8tJRlEKhSX@cluster0.iuny2dr.mongodb.net/
+// FRONTEND_URL= http://localhost:5173
+// JWT_SECRET=palabrasupersecreta
+
+
+// SMTP_HOST=sandbox.smtp.mailtrap.io
+// SMTP_PORT=2525
+// SMTP_USER=e532ff9dd6a741
+// SMTP_PASS=1c6d7d84df227d
+
+
+
+
+//devdjcod
+//A2kElS8tJRlEKhSX
