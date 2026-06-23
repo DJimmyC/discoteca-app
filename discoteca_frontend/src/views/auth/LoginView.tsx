527dfb3d6fdc63298d03c8a6d49d92a2e1cefe38
Jimmy <devdjcod@gmail.com>
2026-06-22 22:49:33 -0400

    Actualizacion de cajas con apertura activa

--- discoteca_frontend/src/views/auth/LoginView.tsx
+++ discoteca_frontend/src/views/auth/LoginView.tsx
@@ -568,14 +568,14 @@ export default function LoginView() {
                 </div>
               </div>
 
-              <nav className="mt-6 text-center">
+              {/* <nav className="mt-6 text-center">
                 <Link
                   to="/auth/registrar"
                   className="inline-flex items-center justify-center rounded-xl px-4 py-2 text-sm font-black text-indigo-600 transition hover:bg-indigo-50 hover:text-indigo-700"
                 >
                   ¿No tienes una cuenta? Crear cuenta
                 </Link>
-              </nav>
+              </nav> */}
             </div>
           </div>
         </div>
